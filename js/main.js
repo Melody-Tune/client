54d0846bdad5f14702de05280f5cc7d36617fccc
Aditya Respati <dityo.hario@gmail.com>
2019-01-18 10:36:49 +0700

    lyric

--- js/main.js
+++ js/main.js
@@ -4,16 +4,37 @@ var app = new Vue({
     message: 'hello from vue',
     host: "http://localhost:3000",
     localStorageToken: localStorage.getItem("token"),
+    artist: "slipknot",
+    title: "duality"
   },
   methods: {
     logout() {
       localStorage.clear()
       this.localStorageToken = ""
     },
+    getLyric: function(){
+      console.log('masuk')
+      let body = {
+        artist: this.artist,
+        title: this.title
+      }
+      console.log(body)
+      axios
+        .post(
+          `${url}/music/lyric`,
+          body
+        )
+        .then(response => {
+          console.log(response)
+          
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
   },
   mounted(){
   },
-  computed: {
-    
-  }
+  computed: {}
 })
+    
