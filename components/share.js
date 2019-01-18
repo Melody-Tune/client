84bfa19a73c64ab75880f30784a729d38ecea047
Ribka Haryuni <ribka.blue@gmail.com>
2019-01-18 11:12:20 +0700

    music url added

--- components/share.js
+++ components/share.js
@@ -2,12 +2,12 @@ Vue.component('share', {
   template:
   `
   <div>
-    <a target="_blank" :href="'https://www.facebook.com/sharer/sharer.php?kid_directed_site=0&sdk=joey&u=' + 'song.url' + '&display=popup&ref=plugin&src=share_button'"> 
+    <a target="_blank" :href="'https://www.facebook.com/sharer/sharer.php?kid_directed_site=0&sdk=joey&u=' + 'music.url' + '&display=popup&ref=plugin&src=share_button'"> 
       Share<i class="material-icons prefix">share</i>
     </a>
   </div>
   `,
-  props: [],
+  props: ['music'],
   methods: {
 
   }
